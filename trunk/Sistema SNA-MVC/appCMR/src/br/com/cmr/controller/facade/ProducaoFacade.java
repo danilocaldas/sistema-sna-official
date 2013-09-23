fa688ce03b3cdbfe2ca849e786c5d6fa2c1a5212
dmcmaumau@gmail.com <dmcmaumau@gmail.com@4c766f45-f05c-2eae-d7cf-5415331f9f93>
2013-09-23 17:56:42 +0000

    app cmr

--- trunk/Sistema SNA-MVC/appCMR/src/br/com/cmr/controller/facade/ProducaoFacade.java
+++ trunk/Sistema SNA-MVC/appCMR/src/br/com/cmr/controller/facade/ProducaoFacade.java
@@ -4,10 +4,37 @@
  */
 package br.com.cmr.controller.facade;
 
+import br.com.cmr.model.dao.IProducaoDAO;
+import br.com.cmr.model.dao.ProducaoDAO;
+import br.com.cmr.model.entity.Producao;
+import java.util.List;
+
 /**
  *
  * @author ritacosta
  */
 public class ProducaoFacade {
+    
+    private IProducaoDAO dAO;
+
+    public ProducaoFacade() {
+        this.dAO = new ProducaoDAO();
+    }
+    
+    public int save(Producao producao){
+        return dAO.save(producao);
+    }
+    
+    public int update(Producao producao){
+        return dAO.update(producao);
+    }
+    
+    public int remove(Long id){
+        return dAO.remove(id);
+    }
+    
+    public List<Producao> findAll(){
+        return dAO.findAll();
+    }
     
 }
